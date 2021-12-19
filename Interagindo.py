946ceff6136fe4b9dd7b64a1ddbf8f92da15b556
renatabezerratech <renatabezerratecnologia@gmail.com>
2021-12-19 15:22:14 -0300

    inicial commit

---
+++ Interagindo.py
@@ -0,0 +1,18 @@
+# Aqui, resolvi praticar a interação com o usuário.
+
+nome=input('Qual é o seu nome?')
+print('Que bom te ver por aqui ',nome)
+idade=input('Qual é a sua idade?')
+print('Com ',idade,' anos ',nome,' você tem muitas coisas legais para aprender!')
+resposta=input('É a primeira vez que você estuda programação?')
+if resposta == 'não':
+    print('Então vamos reforçar os conhecimentos.')
+else:
+    print('Então vamos começar a trilhar sua aprendizagem para se tornar um programador.')
+
+
+
+
+
+
+
---
+++ TestandoTipo.py
@@ -0,0 +1,12 @@
+# Vou criar uma variável para receber um valor digitado pelo usuário;
+# Em seguida, através do comando type, vou descobrir o tipo primitivo (dado básico, nativo da linguagem);
+# Em python, podem ser dados nativos: int, float, str, bool;
+# Ocorrerá um problema: um valor retornado do comando input sempre será srt (string):
+
+x = input('Digite qualquer coisa: ')
+print('O tipo primitivo desse valor é: ', type(x))
+
+# O que fazer para descobrir seu tipo então???
+# Pode fazer testes através de perguntas que retornam um valor bool:
+
+print('É um número? ',x.isnumeric())
