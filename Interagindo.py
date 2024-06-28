894d769a982eb78c8ead64b1da45c1d5dbc2d440
RB <renatabezerratecnologia@gmail.com>
2024-06-28 16:04:50 -0300

    indentando

--- Interagindo.py
+++ Interagindo.py
@@ -3,14 +3,35 @@
 nome=input('Qual é o seu nome?')
 print('Que bom te ver por aqui ',nome)
 idade=input('Qual é a sua idade?')
-print('Com ',idade,' anos ',nome,' você tem muitas coisas legais para aprender!')
-resposta=input('É a primeira vez que você estuda programação?')
+print('Com {} anos {} você tem muitas coisas legais para aprender!'.format(idade,nome))
+resposta=input('Vamos descobrir alguns resultados de operações matemáticas?')
 if resposta == 'não':
-    print('Então vamos reforçar os conhecimentos.')
+    print('ok então!')
 else:
-    print('Então vamos começar a trilhar sua aprendizagem para se tornar um programador.')
+    print('Então vamos trabalhar com dois valores.')
 
+    x=int(input('Digite um valor: '))
+    y=int(input('Digite outro valor: '))
+    s=x+y
+    z=x-y
+    m=x*y
+    d=x/y
+    e=x**2
+    f=y**2
 
+    print('='*30)
+    print('A soma de {} e {} é: {}'.format(x,y,s))
+    print('='*30)
+    print('A subtração de {} e {} é: {}'.format(x,y,z))
+    print('='*30)
+    print('A multiplicação de {} e {} é: {}'.format(x,y,m))
+    print('='*30)
+    print('A divisão de {} e {} é: {}'.format(x,y,d))
+    print('='*30)
+    print('O resultado de {} elevado à 2 é: {}'.format(x,e))
+    print('='*30)
+    print('O resultado de {} elevado à 2 é: {}'.format(y,f))
+    print('='*30)
 
 
 
