fcaeb70348bb5ae581e91ea10cb6ee8f432e1a19
RB <renatabezerratecnologia@gmail.com>
2024-07-11 15:19:13 -0300

    update pandas

---
+++ UsandoPandas.py
@@ -0,0 +1,21 @@
+#-----------------------Algumas anotações--------------------------------------------------------
+# pip install pandas
+# pip install openpyxl numpy
+#A new release of pip is available: 24.1.1 -> 24.1.2
+#[notice] To update, run: C:\Users\rsbez\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe -m pip install --upgrade pip
+
+#------------------------------------------------------------------------------------------------
+# Importante, quando estiver trabalhando com Pandas, lembrar que para o python cada linha é um index
+# Exemplo dentro de um for: for linha in tabela.index:
+# Lembrando que para o python ':' é o comando faça
+#------------------------------------------------------------------------------------------------
+# Para localizar informações use .loc[]
+# Exemplo tabela.loc[linha, coluna]
+#------------------------------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+tabela_x = pd.read_csv('ParaPandas.csv')
+y = tabela_x.count
+print(y)
+
+
